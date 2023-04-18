b28553c9ed35ea0e0e8533d96db603c5e075b8ce
Mikhail <smirnow.mishas2017@gmail.com>
2023-04-18 21:51:06 +0600

    work? but_ignore_2

--- TreeClassifiers/CART/MyCart.py
+++ TreeClassifiers/CART/MyCart.py
@@ -16,7 +16,6 @@ class Node:
     self.features = list(self.X.columns) #признаки
     # это так позже, когда мы делаем наш поиск, чтобы получить лучшее разделение.  мы можем просто индексировать признаки
     self.counts = self.get_counts(self.y)
-    # self.gini = self.get_gini(self.counts)
     self.gini = self.get_gini(self.counts)
 
     self.num_samples = len(self.y)
@@ -33,12 +32,7 @@ class Node:
     p0 = counts[0] / (counts[0] + counts[1] + counts[2])
     p1 = counts[1] / (counts[0] + counts[1] + counts[2])
     p2 = counts[2] / (counts[0] + counts[1] + counts[2])
-    # print("P0", p0, "P1", p1, "P2", p2)
     return 1 - (p0 ** 2 + p1 ** 2 + p2**2)
-
-  def get_possible_splits(self, features): #????
-    unique_values = np.unique(self.X[features])
-    return (unique_values[1:] + unique_values[:-1])/2
 
   def get_split_gini(self, features, split_point):
     #разделили на below above
@@ -47,9 +41,7 @@ class Node:
 
     #посчитали, сколько каждого класса в них  
     below_counts = self.get_counts(below[self.y_col].to_list())
-    # print("BW", below_counts)
     above_counts = self.get_counts(above[self.y_col].to_list())
-    # print("AW", above_counts)
 
     below_weight = len(below) / (len(below) + len(above))
     above_weight = 1 - below_weight
@@ -57,8 +49,6 @@ class Node:
     below_gini = self.get_gini(below_counts)
     above_gini = self.get_gini(above_counts)
     #Прирост инфы
-    # print("PRIROST: ")
-    # print("BW", below_weight, "*", "BG", below_gini, "+", "AW", above_weight, "*", "AG", above_gini)
 
     return (below_weight * below_gini) + (above_weight * above_gini)
 
@@ -66,16 +56,15 @@ class Node:
     opt_feature, opt_split = None, None
     opt_gini_reduction = 0 # We want to maximize this
     for feature in self.features:
-      possible_splits = self.get_possible_splits(feature)
+      # possible_splits = self.get_possible_splits(feature)
+      df_splits = sorted(self.data[feature])
+      possible_splits = df_splits[1: -1]
       for split in possible_splits:
         split_gini = self.get_split_gini(feature, split)
         gini_reduction = self.gini - split_gini
-        # print("gini", self.gini, "minus", split_gini)
-        # print("gini_reduction", gini_reduction)
 
         if gini_reduction > opt_gini_reduction:
            opt_gini_reduction = gini_reduction
-          #  print("opt_gini_reduction", opt_gini_reduction)
            opt_feature, opt_split = feature, split
 
     return opt_feature, opt_split
@@ -84,31 +73,16 @@ class Node:
 
   def make_split(self, max_depth, min_samples_split):
     self.opt_feature, self.opt_split = self.get_opt_split()
-    # print("OPT", self.opt_feature)
     if self.opt_feature is not None:
-        # print("YES")
-        # print("D_MD", self.depth, max_depth)
-        # print("N_MIN", self.num_samples, min_samples_split)
         if (self.depth < max_depth) and (self.num_samples > min_samples_split):
-            # print("YES2")
             below = self.data[self.data[self.opt_feature] < self.opt_split]
             above = self.data[self.data[self.opt_feature] > self.opt_split]
-            # print("B", below)
-            # print("A", above)
             
             self.left = Node(below, 'target', depth = self.depth + 1)
             self.right = Node(above, 'target', depth = self.depth + 1)
-            # print("DEEEEEEEEEEEEEEEEEPTH", self.depth)
             #Reccurence hahahahahah
             self.left.make_split(max_depth, min_samples_split) #????
             self.right.make_split(max_depth, min_samples_split)
-
-
-            # if self.methood == "gini":
-            #   dshadkhjaks
-
-            # if self.methood == "entorpy":
-            #   dsadasd
 
 class CART:  
   def __init__(self, max_depth, min_samples_split, task = None):
@@ -121,28 +95,18 @@ class CART:
 
   def predict_one(self, row):
     node = self.root
-    # n = 0
     while node.depth < self.max_depth:
-      # print("NM", node.num_samples)
       if node.num_samples <= self.min_samples_split:
-        # print('BREAK1')
         break
-      # n += 1
-      # print("N", n)
       split_feature, split_point = node.opt_feature, node.opt_split
 
       if split_feature is None:
-        # print('BREAK2')
         break
       else:
         if row[split_feature] < split_point:
           node = node.left
-          # print(node.left)
-          # print("NODE_LEFT", node.depth)
         else:
           node = node.right
-          # print(node.right)
-          # print("NODE_RIGHT", node.depth)
       return node.predicted_label
 
   def predict_many(self, X):
--- TreeClassifiers/CART/MyCart_debug.py
+++ TreeClassifiers/CART/MyCart_debug.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import json
 
-videogame = '{"console": [0.32, 0.44, 0.22, 0.12, 0.63, 0.111, 0.022, 0.30, 0.61, 0.27, 0.35], "games": [0.34, 0.45, 0.2, 0.121, 0.62, 0.11, 0.02, 0.35,  0.50, 0.16, 0.75], "target": [0, 1, 2, 0, 0, 2, 1, 0, 1, 1, 2]}'
+videogame = '{"console": [0.32, 0.44, 0.22, 0.12, 0.63, 0.111, 0.022, 0.30, 0.61, 0.27, 0.35], "games": [0.34, 0.45, 0.2, 0.121, 0.62, 0.11, 0.02, 0.35,  0.50, 0.16, 0.75], "target": [0, 1, 2, 0, 2, 2, 1, 0, 1, 2, 2]}'
 dict = json.loads(videogame)
 df2 = pd.DataFrame.from_dict(dict)
 
@@ -13,12 +13,12 @@ test2 = df2.drop(train2.index).reset_index(drop = True)
 
 
 #--------------------------Debug-Start--------------------------------------#
-root2 = Node(train2, 'target', depth = 0)
-print(root2.counts)
-print(root2.get_possible_splits('games'))
+# root2 = Node(train2, 'target', depth = 0)
+# print(root2.counts)
+# # print(root2.get_possible_splits('games'))
 
-print(root2.get_opt_split())
-# print(root2.left)
+# print(root2.get_opt_split())
+# # print(root2.left)
 # print(root2.make_split(max_depth = 2, min_samples_split = 3))
 
 #------------------Train----------------------------------#
